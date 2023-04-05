f41e0b31337e1601472dc2975d9e0d0b8483ab78
Rajendran <rajendrans@xbi4.com>
2023-04-05 19:34:17 +0530

    init angrm ckr

---
+++ src/AnagramsForm/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import "./style.css"
+
+export default function AnagramChecker() {
+  const [word1, setWord1] = useState('');
+  const [word2, setWord2] = useState('');
+  const [isAnagram, setIsAnagram] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  function checkAnagram() {
+    if (word1.length !== word1.length) {
+        return false;
+      }
+   
+    const sortedWord1 = word1.split('').sort().join('');
+    const sortedWord2 = word2.split('').sort().join('');
+    setIsAnagram(sortedWord1 === sortedWord2);
+    setSubmitted(true);
+  }
+
+
+  return (
+    <>
+    <div className='main-container'>
+        <div className='anagram-form'>
+            <h1>Anagrams Checker</h1>
+<p>Word1:</p>
+        <input type="text" value={word1} onChange={(e) => setWord1(e.target.value)} />
+        <p>Word2:</p>
+        <input type="text" value={word2} onChange={(e) => setWord2(e.target.value)} />
+        <p></p>
+      <button onClick={checkAnagram}>Check Anagram</button>
+      {submitted && isAnagram && <p>{word1} and {word2} are anagrams!</p>}
+      {submitted && !isAnagram && <p>{word1} and {word2} are not anagrams.</p>}
+      </div>
+    </div>
+    </>
+  );
+}
